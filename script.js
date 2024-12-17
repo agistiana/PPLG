6d409b34bc9d61a04bdea9074366968f3e35699c
Agistiana Nurohman <agistiananurohman@gmail.com>
2024-12-17 20:34:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function myFunction1() {
+  var element = document.querySelector(".prof1 p");
+  element.classList.toggle("deskripsi");
+}
+function myFunction2() {
+  var element = document.querySelector(".prof2 p");
+  element.classList.toggle("deskripsi");
+}
+function myFunction3() {
+  var element = document.querySelector(".prof3 p");
+  element.classList.toggle("deskripsi");
+}
+function myFunction4() {
+  var element = document.querySelector(".prof4 p");
+  element.classList.toggle("deskripsi");
+}
+function myFunction5() {
+  var element = document.querySelector(".prof5 p");
+  element.classList.toggle("deskripsi");
+}
+function myFunction6() {
+  var element = document.querySelector(".prof6 p");
+  element.classList.toggle("deskripsi");
+}
+function myFunction7() {
+  var element = document.querySelector(".prof7 p");
+  element.classList.toggle("deskripsi");
+}
+function myFunction8() {
+  var element = document.querySelector(".prof8 p");
+  element.classList.toggle("deskripsi");
+}
